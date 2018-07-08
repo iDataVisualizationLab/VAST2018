7cc2dd90a7aa70c06d4a0ec8f5beab5624937df5
Pham Van Vung <phamvanvung@gmail.com>
2018-07-07 23:56:56 -0500

    Changed mouseover behavior

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function loadData() {
-    myDataProcessor.readData("mc2/Boonsong Lekagul waterways readings2.csv", dataHandler);
+    myDataProcessor.readData("mc2/Boonsong Lekagul waterways readings.csv", dataHandler);
 }
 
 $(document).ready(() => {
